d3951d981939d158a3b75d778dd975c54e23d019
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2019-05-15 20:16:07 -0700

    Use f-strings for better string formatting

--- run.py
+++ run.py
@@ -6,22 +6,25 @@ from datetime import datetime
 
 # Get absolute path of this script & journal file
 dir_path      = dirname(__file__)
-journal_file  = dir_path + '/journal.md'
+journal_file  = f'{dir_path}/journal.md'
 
 ## Create journal file if not already created.
 if isfile(journal_file) == False:
     print('Journal file not found. Creating...')
     with open(journal_file, 'x') as f:
-        f.write('#')
+        f.write('')
 
 # Open journal in default editor, otherwise open in vim. Have this func take in
 # editor?
 def open_in_pref_editor():
     pref_editor = environ.get('EDITOR')
     if environ.get('EDITOR') == None:
-        system('nvim + ' + journal_file)
+        system(f'nvim + {journal_file}')
     else:
         system(pref_editor + journal_file)
+
+# Search bashrc for alias 'j'. If it doesn't exist, create it.
+alias = f'alias j={dir_path}/run.py'
 
 # Get today's date & compare it to the last mod date of journal_file
 pretty_date   = '%A, %e %B, %Y'
@@ -34,5 +37,5 @@ if today_date == mod_date:
 else:
     print("Creating today's entry...")
     with open(journal_file, 'a') as f:
-        f.write('#' + today_date)
+        f.write(f'# {today_date}')
     open_in_pref_editor()
